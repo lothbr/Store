fad26b6e682a1d3204a111b58c9454bc5868dfdc
lothbr <abrahamolaoluwa3@gmail.com>
2023-08-01 14:07:39 +0100

    Created_Student&Product_Models

---
+++ Store/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String? Name { get; set; }
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
---
+++ Store/Model/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.Model
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public String? Name { get; set; }
+        public String? Class { get; set; }
+        public int Score { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
